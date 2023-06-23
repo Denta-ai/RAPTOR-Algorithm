eb18370454fa534a3f150ea32160d6389d05d906
Denta-ai <septyandenta@gmail.com>
2023-06-23 22:01:09 +0700

    updated power-number.adb

--- pow-numb/power-number.adb
+++ pow-numb/power-number.adb
@@ -1,48 +1,48 @@
-procedure power-number is
-   procedure calculatePow (
-         x : in  ??;
-         y : in  ??_Variable;
-         result : in  ??_Variable)
+procedure Power_Number is
+   procedure Calculate_Pow (
+         x : in  Float;
+         y : in  Integer;
+         result : out Float)
    is
-      i : ??_Variable;
+      i : Integer;
    begin
-      if y=0 then 
-         Put_Line("result = "+result);
+      if y = 0 then 
+         Put_Line("Result = " & Float'Image(result));
       else
-         if y>0 then 
-            i :=0;
+         if y > 0 then 
+            i := 0;
             loop
-               i :=i+1;
-               result :=result*x;
-               exit when i=y;
+               i := i + 1;
+               result := result * x;
+               exit when i = y;
             end loop;
-            Put_Line("the result is "+result);
+            Put_Line("The result is " & Float'Image(result));
          else
-            if y<0 then 
-               i :=0;
+            if y < 0 then 
+               i := 0;
                loop
-                  i :=i+1;
-                  result :=result/x;
-                  exit when i=(y*-1);
+                  i := i + 1;
+                  result := result / x;
+                  exit when i = (y * -1);
                end loop;
-               Put_Line("the result is "+result);
+               Put_Line("The result is " & Float'Image(result));
             else
                Put_Line("Error");
             end if;
          end if;
       end if;
-   end calculatePow;
-   raptor_prompt_variable_zzyz : Unbounded_String;
-   x : ??_Variable;
-   y : ??_Variable;
-   result : ??_Variable;
+   end Calculate_Pow;
+   
+   x : Float;
+   y : Integer;
+   result : Float;
 begin
-   raptor_prompt_variable_zzyz :="Input value x = ";
-   Put_Line(raptor_prompt_variable_zzyz);
+   Put("Input value x = ");
    Get(x);
-   raptor_prompt_variable_zzyz :="Input value y = ";
-   Put_Line(raptor_prompt_variable_zzyz);
+   
+   Put("Input value y = ");
    Get(y);
-   result :=1;
-   calculatePow(x,y,result);
-end power-number;
+   
+   result := 1.0;
+   Calculate_Pow(x, y, result);
+end Power_Number;
